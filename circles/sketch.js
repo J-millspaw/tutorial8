6f2d8187a1c57befc15b6de8675cd0faedc7d190
J-millspaw <jmillspa@unca.edu>
2022-11-04 15:59:36 -0400

    tutorial12

--- circles/sketch.js
+++ circles/sketch.js
@@ -2,14 +2,33 @@ function setup() {
     const canvasWidth = window.innerWidth;
     const canvasHeight = window.innerHeight; 
     createCanvas(canvasWidth, canvasHeight);
-
-    // fill('red');
+    
+    fill('red');
+    
     noFill();
-    circle(100, 200, 50);
-    circle(100, 250, 50);
-    circle(100, 300, 50);
-    circle(100, 350, 50);
-    circle(100, 400, 50);
+    let counter = 0;
+    while (counter < 7) {
+        circle(350, 300 + counter*10, counter * 10);
+        counter += 1}
+
+        while (counter < 7) {
+            circle(350 + counter*10, 300, counter * 10);
+        
+        
+        //if (counter % 2 == 0){
+        //    fill('aquamarine')
+           
+       // } else {fill('darkgreen')}
+        //circle(100 + counter * 10, 200 + counter*25, 50)
+        //circle(275 - counter * 10, 200 + counter*25, 50)
+        //circle(275, 200 + counter*25, 50);
+        counter += 1
+    }
+    //circle(100, 200, 50);
+   // circle(100, 250, 50);
+    //circle(100, 300, 50);
+    //circle(100, 350, 50);
+    //circle(100, 400, 50);
 
     drawGrid(canvasWidth, canvasHeight);
 }
